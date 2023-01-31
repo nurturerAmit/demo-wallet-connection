6b5f38df3c9f8551c78c231c9eb467bfa3347ad1
amit231 <amitsharma0843@gmail.com>
2023-01-31 16:23:22 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { ConnectButton , useAccountModal, useChainModal, useConnectModal} from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
 
 import { Account } from './components'
 
 export function App() {
   const { isConnected } = useAccount()
+  const { openConnectModal } = useConnectModal();
+  const { openAccountModal } = useAccountModal();
+  const { openChainModal } = useChainModal();
   return (
     <>
       <h1>wagmi + RainbowKit + Vite</h1>
-
-      <ConnectButton />
       {isConnected && <Account />}
+      <ConnectButton/>
     </>
   )
 }
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,10 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createClient } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
+import { arbitrumGoerli, goerli, mainnet } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
+  [arbitrumGoerli],
   [
     publicProvider(),
   ],
